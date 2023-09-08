da125601fc6cb14b00080e9544bd1c520da36bef
Andrew Yatsura <andrewyazura203@gmail.com>
2023-09-09 01:34:13 +0300

    WIP: move .streamlit folder to app folder

--- psychology_qa/app.py
+++ psychology_qa/app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 from st_pages import add_page_title, show_pages_from_config
 
 from authenticator import display_authentication_controls
@@ -5,3 +6,10 @@ from authenticator import display_authentication_controls
 add_page_title()
 show_pages_from_config()
 display_authentication_controls()
+
+query = st.chat_input("Ask a psychology-related question")
+
+if not query:
+    st.stop()
+
+st.write(query)
