218f2f928ca3f540329a5843a3bac906537e6341
Andrew Yatsura <andrewyazura203@gmail.com>
2023-09-10 19:24:23 +0300

    WIP: required book's author and title when uploading a book

--- psychology_qa/pages/upload_audio.py
+++ psychology_qa/pages/upload_audio.py
@@ -3,10 +3,13 @@ from typing import TYPE_CHECKING
 
 import librosa as lr
 import streamlit as st
+from peewee import PeeweeException
 from st_pages import add_page_title, show_pages_from_config
 
 from authenticator import display_authentication_controls
 from constants import LANGUAGES, SAMPLE_RATE, WHISPER_MODELS
+from database import init_database
+from models import Author, Book
 
 if TYPE_CHECKING:
     from streamlit.runtime.uploaded_file_manager import UploadedFile
@@ -14,10 +17,19 @@ if TYPE_CHECKING:
 add_page_title()
 show_pages_from_config()
 display_authentication_controls()
+init_database()
+
+AUTHORS_NAME_TO_ID = {
+    author.name: author.id for author in Author.select(Author.name)
+}
 
 files_form = st.empty()
-
 with files_form.form("files"):
+    author_name = st.selectbox(
+        "Select book's author", AUTHORS_NAME_TO_ID.keys()
+    )
+    book_title = st.text_input("Enter book's title", max_chars=255)
+
     language = st.selectbox("Select language of the audiobook", LANGUAGES)
     model_name = st.selectbox("Select a whisper model", WHISPER_MODELS)
 
@@ -27,9 +39,17 @@ with files_form.form("files"):
         type=["wav", "mp3", "m4a"],
     )
 
-    files_submitted = st.form_submit_button("Upload", use_container_width=True)
+    form_submitted = st.form_submit_button("Upload", use_container_width=True)
 
-if not files_submitted:
+if not form_submitted:
+    st.stop()
+
+if author_name is None:
+    st.error("Author is required")
+    st.stop()
+
+if book_title is None or book_title.strip == "":
+    st.error("Book's title is required")
     st.stop()
 
 if not files:
@@ -37,6 +57,16 @@ if not files:
     st.stop()
 
 files_form.empty()
+
+try:
+    book = Book.create(
+        author_id=AUTHORS_NAME_TO_ID[author_name],
+        title=book_title.strip(),
+    )
+
+except PeeweeException:
+    st.error("Failed to add a book")
+    st.stop()
 
 with st.spinner("Loading Whisper model..."):
     from pipelines.whisper import get_whisper_pipeline
@@ -68,6 +98,8 @@ with tempfile.NamedTemporaryFile(suffix=".txt") as temp:
         from pipelines.processing import get_processing_pipeline
 
         pipe = get_processing_pipeline(language)
-        documents = pipe.run(file_paths=[temp.name])
+        documents: dict = pipe.run(
+            file_paths=[temp.name], meta={"book_id": book.id}
+        )
 
-st.json(documents)
+st.json([document.to_dict() for document in result["documents"]])
--- psychology_qa/pages/upload_book.py
+++ psychology_qa/pages/upload_book.py
@@ -2,10 +2,13 @@ import tempfile
 from typing import TYPE_CHECKING
 
 import streamlit as st
+from peewee import PeeweeException
 from st_pages import add_page_title, show_pages_from_config
 
 from authenticator import display_authentication_controls
 from constants import LANGUAGES
+from database import init_database
+from models import Author, Book
 
 if TYPE_CHECKING:
     from streamlit.runtime.uploaded_file_manager import UploadedFile
@@ -13,10 +16,17 @@ if TYPE_CHECKING:
 add_page_title()
 show_pages_from_config()
 display_authentication_controls()
+init_database()
+
+AUTHORS_NAME_TO_ID = {author.name: author.id for author in Author.select()}
 
 files_form = st.empty()
+with st.empty().form("files"):
+    author_name = st.selectbox(
+        "Select book's author", AUTHORS_NAME_TO_ID.keys()
+    )
+    book_title = st.text_input("Enter book's title", max_chars=255)
 
-with files_form.form("files"):
     language = st.selectbox("Select book's language", LANGUAGES)
 
     file: "UploadedFile" = st.file_uploader(
@@ -24,9 +34,18 @@ with files_form.form("files"):
         type=["txt", "pdf", "md", "doc", "docx"],
     )
 
-    files_submitted = st.form_submit_button("Upload", use_container_width=True)
+    form_submitted = st.form_submit_button("Upload", use_container_width=True)
 
-if not files_submitted:
+if not form_submitted:
+    st.stop()
+
+if author_name is None:
+    st.error("Author is required")
+    st.stop()
+
+print(book_title)
+if book_title is None or book_title.strip() == "":
+    st.error("Book's title is required")
     st.stop()
 
 if file is None:
@@ -35,6 +54,16 @@ if file is None:
 
 files_form.empty()
 
+try:
+    book = Book.create(
+        author_id=AUTHORS_NAME_TO_ID[author_name],
+        title=book_title.strip(),
+    )
+
+except PeeweeException:
+    st.error("Failed to add a book")
+    st.stop()
+
 with tempfile.NamedTemporaryFile(suffix=file.name) as temp:
     temp.write(file.read())
 
@@ -42,6 +71,8 @@ with tempfile.NamedTemporaryFile(suffix=file.name) as temp:
         from pipelines.processing import get_processing_pipeline
 
         pipe = get_processing_pipeline(language)
-        documents = pipe.run(file_paths=[temp.name])
+        result: dict = pipe.run(
+            file_paths=[temp.name], meta={"book_id": book.id}
+        )
 
-st.json(documents)
+st.json([document.to_dict() for document in result["documents"]])
